64c78a1d0d9818f514ca6ae8d22afe82371381c0
Chavira-cloud <markos.mgcg@gmail.com>
2025-07-08 17:01:08 -0700

    Avance tarde 08-07-25

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ nuevoEnlace.textContent='nuevo enlace';
 nuevoEnlace.classList.add('navegacion__enlace');
 
 //Agregarlo en el documento
-const navegacion =document.querySelector('.navegacion');
+const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
 
 console.log(nuevoEnlace);
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function(){ // solo espera por el 
 console.log(5);
 
 //Seleccionar elementos y asociarlos a un evento
-
+/*
 const btnEnviar = document.querySelector('.boton--primario');
 btnEnviar.addEventListener('click', function(e){
     console.log(e);
@@ -65,3 +65,88 @@ btnEnviar.addEventListener('click', function(e){
 
     console.log('enviando formulario');
 });
+*/
+
+//eventos de los inputs y textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario')
+
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+//El evento de submit
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //validas el formulario
+    const {nombre, email, mensaje} = datos;
+
+    if(nombre === '' || email === '' || mensaje === ''){
+        mostraAlerta('Todos los campos SON OBLIGATORIOS','error')
+        return; // Corta la ejecución del código   
+    }
+    
+    // crear una alerta para el Envio correctamente
+    mostraAlerta('Mensaje enviado correctamente')
+        
+    console.log('Enviando formulario');
+});
+
+function leerTexto(e){
+    //console.log(e.target.value);
+    datos[e.target.id] = e.target.value;
+
+   //console.log(datos);   
+}
+
+function mostraAlerta(mensaje, error = null){
+    const alerta = document.createElement('P')
+    alerta.textContent = mensaje;
+
+    if(error){
+        alerta.classList.add('error')
+    }else{
+        alerta.classList.add('frmcontact');
+    }
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() =>{
+        alerta.remove();
+    },5000)
+}
+
+/*
+function mostrarMensaje(mensaje){
+    const alerta = document.createElement('P')
+    alerta.textContent = mensaje;
+    alerta.classList.add('frmcontact');
+    formulario.appendChild(alerta)
+
+    setTimeout(()=>{
+        alerta.remove();
+    },5000)
+}
+
+// Muestra un erro en pantalla
+function mostrarError(mensaje){
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error')
+    formulario.appendChild(error)
+
+    //Desaparezca en despues de 5 seg
+    setTimeout(()=>{
+        error.remove();
+    },5000);
+}
+*/
